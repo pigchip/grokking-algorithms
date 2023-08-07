020e88c5cea31e676215cf69e34d408de082d407
pigchip <guzmancruzandresmiguel@gmail.com>
2023-08-06 22:38:10 -0600

    Add selection-sort algorithm

--- binary-search.py
+++ binary-search.py
@@ -1,19 +1,22 @@
-def binary_search(list, item):
+#BINARY-SEARCH
+#GETTING AN ORDER ARRAY OR LIST AND AN ELEMENT TO FIND ON THE LIST
+def binary_search(list,value):
+    #GET THE LOW AND HIGH VARIABLES TO KEEP TRACK OF THE LIMITS THAT WE WILL BE SEARCHING ON THE ARRAY
     low = 0
-    high = len(list)-1
-
+    high = len(list) - 1
+    #SEARCHING ON ALL ELEMENTS
     while low <= high:
-        mid = (low+high) // 2
-        guess = list[mid]
-        if guess == item:
-            return mid
-        if guess > item:
-            high = mid - 1
+        index = (low+high) // 2
+        guess = list[index]
+        #WE ALREADY GUESS THE ELEMENT
+        if guess == value:
+            return index
+        if guess > value:
+            high = index - 1
         else:
-            low = mid + 1
+            low = index + 1
     return None
-
-my_list = [1,3,5,7,9]
-
-print(binary_search(my_list, 3))
-print(binary_search(my_list, -1))
+    
+my_list = [1,20,34,55,100]
+print(binary_search(my_list,55))
+print(binary_search(my_list,2))
